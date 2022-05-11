4740af839ab010b3bb1b6ef14d7715368d500dea
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-11 23:47:57 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './pages/Header/Header';
 import Home from './pages/Home/Home';
 import Footer from './pages/Footer/Footer';
+import Login from './pages/Login/Login/Login';
+import Blogs from './pages/Blogs/Blogs';
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
       <Header></Header>
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
      </Routes>
      <Footer></Footer>
     </div>
--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h3>this header component</h3>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">Smart Phones Shop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link as={Link} to="/login" >
+                                Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
