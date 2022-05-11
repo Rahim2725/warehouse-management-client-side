cbd947b867d55e3ec79f43832e8e974b143cf2ca
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-12 02:04:46 +0600

    header update

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar className='shadow mb-5 ' collapseOnSelect expand="lg" bg="light">
                 <Container>
                     <Navbar.Brand as={Link} to="/">Smart Phones Shop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -15,8 +16,12 @@ const Header = () => {
                             <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link as={Link} to="/login" >
+                            <>
+                                <Nav.Link as={Link} to="/mangeItem">Mange-Item</Nav.Link>
+                                <Nav.Link as={Link} to="/addItem">Add-Item</Nav.Link>
+                                <Nav.Link as={Link} to="/myItem">My-Item</Nav.Link>
+                                <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
+                            </> <Nav.Link as={Link} to="/login" >
                                 Login
                             </Nav.Link>
                         </Nav>
