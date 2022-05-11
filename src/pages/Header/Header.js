80a75c1a6127e24f8df7053a20c2c071df742992
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-12 03:27:02 +0600

    register done

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,9 +1,21 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Header.css'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const logout = () => {
+
+        signOut(auth).then(() => {
+            // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
     return (
         <div>
             <Navbar className='shadow mb-5 ' collapseOnSelect expand="lg" bg="light">
@@ -16,14 +28,19 @@ const Header = () => {
                             <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                         </Nav>
                         <Nav>
-                            <>
-                                <Nav.Link as={Link} to="/mangeItem">Mange-Item</Nav.Link>
-                                <Nav.Link as={Link} to="/addItem">Add-Item</Nav.Link>
-                                <Nav.Link as={Link} to="/myItem">My-Item</Nav.Link>
-                                <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
-                            </> <Nav.Link as={Link} to="/login" >
-                                Login
-                            </Nav.Link>
+                            {
+                                user ?
+                                    <>
+                                        <Nav.Link as={Link} to="/mangeItem">Mange-Item</Nav.Link>
+                                        <Nav.Link as={Link} to="/addItem">Add-Item</Nav.Link>
+                                        <Nav.Link as={Link} to="/myItem">My-Item</Nav.Link>
+                                        <Nav.Link onClick={logout} as={Link} to="/logout">Logout</Nav.Link>
+                                    </>
+                                    :
+                                    <Nav.Link as={Link} to="/login" >
+                                        Login
+                                    </Nav.Link>
+                            }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
