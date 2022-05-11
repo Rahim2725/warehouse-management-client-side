05b0414e2b5055ccfacd31d626f85dd6c4b5c449
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-12 03:19:52 +0600

    update register

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDYhT4n-S5qw2GxggKOMulJhFcMFswY0CI",
-  authDomain: "warehouse-managment-27bf4.firebaseapp.com",
-  projectId: "warehouse-managment-27bf4",
-  storageBucket: "warehouse-managment-27bf4.appspot.com",
-  messagingSenderId: "878204909788",
-  appId: "1:878204909788:web:64b0d6d30d6d8714049c19"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth ;
--- src/pages/Login/Register/Register.js
+++ src/pages/Login/Register/Register.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+      const createUser = event => {
+          event.preventDefault();
+          const name = event.target.name.value ;
+          const email = event.target.email.value ;
+          const password = event.target.password.value ;
+
+          createUserWithEmailAndPassword(email, password);
+
+      }
+
+      if(user){
+          navigate('/home')
+      }
+
+
     return (
         <div className='container w-25'>
             <h2 className='my-3'>Please Register??</h2>
-            <form>
-                <input type="text" className='w-100 mb-3 p-2' name="name" placeholder='Name' required />
+            <form onSubmit={createUser}>
+                <input  type="text" className='w-100 mb-3 p-2' name="name" placeholder='Name' required />
                 <br />
                 <input type="email" className='w-100 mb-3 p-2' name="email" id="" placeholder='Email' required />
                 <br />
@@ -15,6 +41,7 @@ const Register = () => {
                 <input type="submit" className='w-100 mb-3 p-2' value="Register" />
             </form>
             <p>Already Have an account ? Please <Link to="/login">Login</Link></p>
+            <p>{error?.message}</p>
         </div>
     );
 };
